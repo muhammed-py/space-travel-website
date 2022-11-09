95e1d406998e449bdb26bca07000ea1f00219a0a
Mohammed Al-Kawaz <84942933+muhammed-py@users.noreply.github.com>
2022-11-09 14:41:34 +0300

    homepage

---
+++ homepage.js
@@ -0,0 +1,18 @@
+const earth = document.querySelector('.earth'),
+      hiddenElement = document.querySelector('.hidden');
+
+
+
+
+
+window.addEventListener('scroll', function(event) {
+    var scroll = this.scrollY;
+    console.log(scroll)
+    if (scroll >= 50) { 
+        earth.classList.add('earth_scroll')
+    } else {earth.classList.remove('earth_scroll')}
+
+    if (scroll >= 50) {
+        hiddenElement.classList.add('show');
+    } else { hiddenElement.classList.remove('show'); }
+})
