629e1ff6975b5f7fae52441aeddfb42791f4fc51
Mohammed Al-Kawaz <84942933+muhammed-py@users.noreply.github.com>
2022-11-09 15:00:51 +0300

    adding more items

--- homepage.js
+++ homepage.js
@@ -1,16 +1,16 @@
 const earth = document.querySelector('.earth'),
-      hiddenElement = document.querySelector('.hidden');
+    hiddenElement = document.querySelector('.hidden');
 
 
 
 
 
-window.addEventListener('scroll', function(event) {
+window.addEventListener('scroll', function (event) {
     var scroll = this.scrollY;
     console.log(scroll)
-    if (scroll >= 50) { 
+    if (scroll >= 50) {
         earth.classList.add('earth_scroll')
-    } else {earth.classList.remove('earth_scroll')}
+    } else { earth.classList.remove('earth_scroll') }
 
     if (scroll >= 50) {
         hiddenElement.classList.add('show');
