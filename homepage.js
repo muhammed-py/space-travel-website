1df13e67db7fe5a353ba63b8b51e2ecda548c907
Mohammed Al-Kawaz <84942933+muhammed-py@users.noreply.github.com>
2022-11-10 11:42:51 +0300

    adding more items

--- homepage.js
+++ homepage.js
@@ -1,9 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-
-
-
 gsap.to('.section_2', {
     scrollTrigger: {
         trigger: '.section_2',
@@ -11,8 +8,8 @@ gsap.to('.section_2', {
         end:' top top',
         scrub: 3,
     },
-    y:-200,
-    duration: 3,
+    y:-100,
+    duration: 1,
 })
 
 gsap.to('.text_1', {
@@ -23,18 +20,44 @@ gsap.to('.text_1', {
     },
     x: 0,
     opacity:1,
-    duration: 3,
+    duration: 1,
 })
 
 gsap.to('.text_2', {
     scrollTrigger: {
         trigger: '.text_1',
         toggleActions: 'restart pause reverse reset',
+        start:'top 25%',
         scrub: 1,
     },
     opacity: 1,
     duration: 1,
 })
+
+
+gsap.to('.section_3', {
+    scrollTrigger: {
+        trigger: '.text_2',
+        toggleActions: 'restart pause reverse reverse',
+        endTrigger:'.section_3',
+        end:'top center',
+        scrub: 3,
+    },
+    y: -250,
+    duration: 3,
+})
+
+gsap.to('.section_4', {
+    scrollTrigger: {
+        trigger: '.section_4',
+        toggleActions: 'restart pause reverse none',
+        ennd: 'center center',
+        scrub: 3,
+    },
+    opacity:1,
+    duration: 3,
+})
+
 
 
 
